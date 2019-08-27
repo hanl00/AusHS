befd318a7c64a38f7fbdb55f6c145332829b2066
nick.han <35565271+nickhan123@users.noreply.github.com>
2019-08-27 12:42:53 +0800

    Update australian_highschool_scrape.py
    implemented writing collected links into an excel file

--- australian_highschool_scrape.py
+++ australian_highschool_scrape.py
@@ -12,7 +12,7 @@ from selenium import webdriver
 from unidecode import unidecode
 
 # dynamic pathname based on different device, instead of hard coding the pathname
-uniqueLinkList_path = os.path.join(os.getcwd(), 'UniqueLinkList_.csv')
+uniqueLinkList_path = os.path.join(os.getcwd(), 'UniqueLinkList.csv')
 extractedData_path = os.path.join(os.getcwd(), 'ExtractedData.csv')
 
 # Setup Chrome display
@@ -27,31 +27,38 @@ user = UserAgent().random
 headers = {'User-Agent': user}
 
 def collect_school_links(str_link):
-    options.add_argument(f'user-agent={user}')
-    options.add_argument('--disable-gpu')
-    # options.add_argument('--headless')
-    driver = webdriver.Chrome(options=options, executable_path=r'C:\Users\Nicholas\Documents\Summer intern @ Seeka\chromedriver.exe')
-    driver.get(str_link)  # This will open the page using the URL
+
+    with open(uniqueLinkList_path, 'wt',encoding='utf-8', newline='') as Linklist:
+        writer2 = csv.writer(Linklist)
+        options.add_argument(f'user-agent={user}')
+        options.add_argument('--disable-gpu')
+        # options.add_argument('--headless')
+        driver = webdriver.Chrome(options=options, executable_path=r'C:\Users\Nicholas\Documents\Summer intern @ Seeka\chromedriver.exe')
+        driver.get(str_link)  # This will open the page using the URL
+
     #content = driver.page_source.encode('utf-8').strip()
     #page = requests.get(str_link, timeout=10, headers=headers)
     #soup = BeautifulSoup(page.text, "lxml")
 
-    driver.find_element_by_link_text('»').click()
+        total_link_info = ['']
 
-    while True:
+        while True:
 
-        soup = BeautifulSoup(driver.page_source, 'lxml')
-        for a in soup.find_all('div', class_='row row-padding-10'):
-            for x in a.find_all('div', class_='col-md-12 clear-fix'):
-                b = x.find('a')
-                print(b['href'])
-
-        try:
-            driver.find_element_by_link_text('»').click()
-            print("Clicked on the next page")
-            continue
-        except:
-            break
+            soup = BeautifulSoup(driver.page_source, 'lxml')
+            for a in soup.find_all('div', class_='row row-padding-10'):
+                for x in a.find_all('div', class_='col-md-12 clear-fix'):
+                    b = x.find('a')
+                    print(b['href'])
+                    institution_link = Homepage + b['href']
+                    total_link_info[0] = institution_link
+                    writer2.writerow(total_link_info)
+                    time.sleep(1)
+            try:
+                driver.find_element_by_link_text('»').click()
+                print("Clicked on the next page")
+                continue
+            except:
+                break
 
 
 collect_school_links("https://www.goodschools.com.au/compare-schools/search?state=ACT")
